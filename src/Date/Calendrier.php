a9bf9528aedd01792c21d4fe2d4614229f018073
Carli Alexandre <carli.alex.13@gmail.com>
2020-07-18 11:56:45 +0200

    Premier Commit

--- src/Date/Calendrier.php
+++ src/Date/Calendrier.php
@@ -6,25 +6,60 @@ use DateTime;
 use DateInterval;
 
 class Calendrier
-{
+{    
+    /**
+     * Premier jour de l'année
+     *
+     * @return DateTime
+     */
     public function FirstDayYear (): DateTime
     {
         return new DateTime('2020-01-01');
     }
-
+    
+    /**
+     * Dernier jour de l'année
+     *
+     * @return DateTime
+     */
     public function LastDayYear(): DateTime
     {
         return new DateTime('2020-12-31');
     }
-
+    
+    /**
+     * Nombre de jour dans l'annnée
+     *
+     * @return DateInterval
+     */
     public function NumberDayYear (): DateInterval
     {
         return date_diff($this->FirstDayYear(), $this->LastDayYear());
     }
-
+    
+    /**
+     * Weeks
+     *
+     * @param  int $data
+     * @return DateTime
+     */
     public function Weeks (): DateTime
     {
-        return $this->FirstDayYear()->modify('+1 week');
+        return $this->FirstDayYear()->add(new DateInterval('P01W'));
+    }
+
+    public function FormatWeek(): string
+    {
+        return $this->Weeks()->format('W');
+    }
+
+    public function DayOfWeek(): string
+    {
+        for($i = 0 ; $i < 7; $i++){
+            $firstDays = $this->Weeks()->modify('last monday')->add(new DateInterval('P0' . $i . 'D'))->format('l d');
+            echo "<th> $firstDays </th>";
+        }      
+        return $firstDays;
     }
 
 }
--- views/calendrier/planning.php
+++ views/calendrier/planning.php
@@ -1,10 +1,14 @@
-<?php
-for($i = 0; $i <=52; $i++):
-    $test1 = $test->FirstDayYear()->add(new DateInterval("P".$i."W"))->format('W') ?>
-    <div class="calendar__day">Semaine N° <?= $test1 ; ?></div>
-        <?php foreach($test1 as $day): ?>
+
+
+        Jours de la Semaine <?= $test->FormatWeek(); ?>
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <?= $test->DayOfWeek(); ?>
+                </tr>
+            </thead>
+        </table>
         
-        <?php endforeach; ?>
-<?php endfor; ?>
+
 
 
